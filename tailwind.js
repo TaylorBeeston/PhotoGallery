896fa638fa308eeefe233859a410e41e21a0b462
TaylorBeeston <beeston.taylor@gmail.com>
2020-07-02 19:54:33 -0700

    fix: have purgecss check ts and tsx files for styles

--- tailwind.js
+++ tailwind.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.js'],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+  ],
   target: 'relaxed',
   prefix: '',
   important: false,
