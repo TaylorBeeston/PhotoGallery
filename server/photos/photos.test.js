5df959857cac35833675fb1252796a224921a7b5
TaylorBeeston <beeston.taylor@gmail.com>
2020-07-31 07:37:32 -0700

    fix: fix server tests

--- server/photos/photos.test.js
+++ server/photos/photos.test.js
@@ -1,15 +1,21 @@
-import MongodbMemoryServer from 'mongodb-memory-server';
+/* eslint-disable no-unused-expressions */
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import request from 'supertest';
-import app from '../app';
 import Photo from './photo.model';
 
+jest.setTimeout(60000);
+
 describe('/api/photos tests', () => {
-  const mongod = new MongodbMemoryServer();
+  const mongod = new MongoMemoryServer();
 
   beforeAll(async () => {
     const uri = await mongod.getConnectionString();
-    await mongoose.connect(uri, { useNewUrlParser: true });
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
   });
 
   afterAll(async () => {
@@ -18,19 +24,43 @@ describe('/api/photos tests', () => {
   });
 
   afterEach(async () => {
-    await Photo.remove({});
+    await Photo.deleteMany({});
   });
 
-  it('should post and get a photo', async () => {
-    const postResponse = await request(app)
-      .post('/api/photos')
-      .send({ name: 'Test Photo', url: 'example.com' });
-    expect(postResponse.status).toBe(200);
+  it('should require a name and url', async () => {
+    const allFields = new Photo({
+      name: 'test',
+      url: 'test.com',
+      thumbnailUrl: 'test.com/2',
+      date: Date.now,
+    });
+    expect(allFields).toBeValid;
 
-    const getResponse = await request(app).get('/api/photos');
-    expect(getResponse.status).toBe(200);
-    expect(getResponse.body).toEqual([
-      expect.objectContaining({ name: 'Test Photo', url: 'example.com' }),
-    ]);
+    const nameAndUrlOnly = new Photo({ name: 'testPhoto', url: 'example.com' });
+    expect(nameAndUrlOnly).toBeValid;
+
+    const noUrl = new Photo({ name: 'testPhoto', thumbnailUrl: 'example.com' });
+    expect(noUrl).not.toBeValid;
+
+    const noName = new Photo({ url: 'test.com', thumbnailUrl: 'example.com' });
+    expect(noName).not.toBeValid;
+  });
+
+  it('should have a unique name', async () => {
+    Photo.create({ name: 'testPhoto', url: 'example.com' });
+
+    const sameName = new Photo({ name: 'testPhoto', url: 'test.com' });
+    expect(sameName).not.toBeValid;
+
+    const differentName = new Photo({
+      name: 'differentName',
+      url: 'example.com',
+    });
+    expect(differentName).toBeValid;
+  });
+
+  it('should always have a date', async () => {
+    const photo = Photo.create({ name: 'testPhoto', url: 'example.com' });
+    expect(photo.date).toBeDefined;
   });
 });
--- server/users/user.test.js
+++ server/users/user.test.js
@@ -1,13 +1,21 @@
-import MongodbMemoryServer from 'mongodb-memory-server';
+/* eslint-disable no-unused-expressions */
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import User from './user.model';
 
+jest.setTimeout(60000);
+
 describe('User model tests', () => {
-  const mongod = new MongodbMemoryServer();
+  const mongod = new MongoMemoryServer();
 
   beforeAll(async () => {
     const uri = await mongod.getConnectionString();
-    await mongoose.connect(uri, { useNewUrlParser: true });
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
   });
 
   afterAll(async () => {
@@ -16,13 +24,63 @@ describe('User model tests', () => {
   });
 
   afterEach(async () => {
-    await User.remove({});
+    await User.deleteMany({});
+  });
+
+  it('should enforce minimum character lengths', async () => {
+    const shortName = new User({ username: 'short', password: 'password' });
+    expect(shortName).not.toBeValid;
+
+    const shortPassword = new User({ username: 'longName', password: 'short' });
+    expect(shortPassword).not.toBeValid;
+  });
+
+  it('should require username and password', async () => {
+    const noName = new User({ password: 'password' });
+    expect(noName).not.toBeValid;
+
+    const noPassword = new User({ username: 'longName' });
+    expect(noPassword).not.toBeValid;
+  });
+
+  it('should enforce unique usernames but not passwords', async () => {
+    User.create({ username: 'validName', password: 'password' });
+
+    const differentPassword = new User({
+      username: 'validName',
+      password: 'different',
+    });
+    expect(differentPassword).not.tobeValid;
+
+    const differentUsername = new User({
+      username: 'differentName',
+      password: 'password',
+    });
+    expect(differentUsername).toBeValid;
   });
 
   it('should hash password on create and update', async () => {
-    const user = new User({ username: 'test', password: 'password' });
-    await user.save();
+    const user = await User.create({
+      username: 'testName',
+      password: 'password',
+    });
 
     expect(user.password).not.toBe('password');
+
+    await User.findOneAndUpdate(
+      { username: 'testname' },
+      { password: 'different' },
+    );
+    expect(user.password).not.toBe('different');
+  });
+
+  it('should be able to verify correct password', async () => {
+    const user = await User.create({
+      username: 'testName',
+      password: 'password',
+    });
+
+    expect(user.isCorrectPassword('different')).toBeFalsy;
+    expect(user.isCorrectPassword('password')).toBeTruthy;
   });
 });
